84c9f171cd9beeb4b8396c089bbf43e758554ca9
OisinRyan22 <sba23149@student.cct.ie>
2024-03-18 16:00:45 +0000

    Main method
    Working on new ways of gaining the data and outputing it to .txt, csv or main console

--- src/integratedca_1/IntegratedCA_1.java
+++ src/integratedca_1/IntegratedCA_1.java
@@ -4,6 +4,10 @@
  */
 package integratedca_1;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  *
  * @author ocean
@@ -13,24 +17,74 @@ public class IntegratedCA_1 {
     /**
      * @param args the command line arguments
      */
+    private static void writeToTxtFile(String report, String filename) {
+        try ( PrintWriter out = new PrintWriter(filename)) {
+            out.println(report);
+        } catch (FileNotFoundException e) {
+            System.err.println("error writing to TXT file: " + e.getMessage());
+        }
+    }
+
+    private static void writeToCsvFile(String report, String filename) {
+        try ( PrintWriter out = new PrintWriter(filename)) {
+            out.println(report);
+        } catch (FileNotFoundException e) {
+            System.err.println("Error writing to CSV file" + e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         Database_Connection dbConnection = new Database_Connection();
-        
+
         CourseReport courseReport = new CourseReport(dbConnection);
         StudentReport studentReport = new StudentReport(dbConnection);
         LecturerReport lecturerReport = new LecturerReport(dbConnection);
+
+//        //Gathers and displays course report data
+//        String courseReportData = courseReport.generateReport();
+//        System.out.println("Course Report:\n" + courseReportData);
+//        
+//        //Gathers and displays student report data
+//        String studentReportData = studentReport.generateReport();
+//        System.out.println("\nStudent Report\n" + studentReportData);
+//        
+//        //Gathers and displays lecturers report data
+//        String lecturerReportData = lecturerReport.generateReport();
+//        System.out.println("\nLecturer Report\n" + lecturerReportData);
+        System.out.println("Select the report you want to generate: ");
+        System.out.println("1. Course Report");
+        System.out.println("2. Student Report");
+        System.out.println("3. Lecturer Report");
+        Scanner sc = new Scanner(System.in);
+        int reportChoice = sc.nextInt();
+
+        String reportData = "";
+        String reportType = "";
+
+        switch (reportChoice) {
+            case 1:
+                reportData = courseReport.generateReport();
+                reportType = "CourseReport";
+                break;
+
+            case 2: 
+                reportData = studentReport.generateReport();
+                reportType = "StudentReport";
+                break;
+                
+            case 3: 
+                reportData = lecturerReport.generateReport();
+                reportType = "LecturerReport";
+                break;
+            default:
+                System.out.println("Invalid report choice.");
+                return;
+        }
         
-        //Gathers and displays course report data
-        String courseReportData = courseReport.generateReport();
-        System.out.println("Course Report:\n" + courseReportData);
-        
-        //Gathers and displays student report data
-        String studentReportData = studentReport.generateReport();
-        System.out.println("\nStudent Report\n" + studentReportData);
-        
-        //Gathers and displays lecturers report data
-        String lecturerReportData = lecturerReport.generateReport();
-        System.out.println("\nLecturer Report\n" + lecturerReportData);
+        System.out.println("Choose the output format: \n1. Console\n2. TXT File\n3. CSV File");
+        int formatChoice = sc.nextInt
+                
+        }
     }
-    
+
 }
