672b42452da266dffb593f5f8f4ed92bd076e59c
OisinRyan22 <sba23149@student.cct.ie>
2024-03-18 13:34:29 +0000

    Database
    Connecting netbeans to mysql database

--- src/integratedca_1/Database_Connection.java
+++ src/integratedca_1/Database_Connection.java
@@ -4,10 +4,24 @@
  */
 package integratedca_1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author ocean
  */
 public class Database_Connection {
+    private final String DB_URL = "jdbc:mysql://localhost";         //database URL
+    private final String user = "root";     //Database username
+    private final String password = "Gethelp33!";       //database password
     
+    public Connection connect() {
+        try {
+            return DriverManager.getConnection(DB_URL, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to database", e);
+        }
+    }
 }
