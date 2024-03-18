92da7bd067c1ec6ed32381656fa9266971bfd5cf
OisinRyan22 <sba23149@student.cct.ie>
2024-03-18 15:31:22 +0000

    Student and Lecture report
    Student and lecturers reports are print from the database tables i have in mysql

--- src/integratedca_1/LecturerReport.java
+++ src/integratedca_1/LecturerReport.java
@@ -4,6 +4,11 @@
  */
 package integratedca_1;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author ocean
@@ -17,6 +22,33 @@ public class LecturerReport implements Report_Inter {
 
     @Override
     public String generateReport() {
-        return "Lecturer report data";
+    StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Lecturer Report:\n");
+        
+        String query = "SELECT name, role, classes_taught, students_count FROM lecturers";
+        
+         try (Connection conn = dbConnection.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+             
+             while (rs.next()) {
+                 String name = rs.getString("name");
+                 String role = rs.getString("role");
+                 String classesTaught = rs.getString("classes_taught");
+                 int studentsCount = rs.getInt("students_count");
+                 
+                 
+                 reportBuilder.append("Name: ").append(name)
+                         .append("\nRole: ").append(role)
+                         .append("\nClasses Taught: ").append(classesTaught)
+                         .append("\nStudents Count: ").append(studentsCount)
+                         .append("\n\n");
+                         
+              }
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
+        return reportBuilder.toString();
     }
-}
+}   
+    
--- src/integratedca_1/StudentReport.java
+++ src/integratedca_1/StudentReport.java
@@ -4,6 +4,11 @@
  */
 package integratedca_1;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author ocean
@@ -17,6 +22,36 @@ public class StudentReport implements Report_Inter {
 
     @Override
     public String generateReport() {
-        return "Student report data";
+        StringBuilder reportBuilder = new StringBuilder();
+        reportBuilder.append("Student Report:\n");
+        
+        String query = "SELECT name, student_number, programme, modules_enrolled, modules_completed, modules_to_repeat FROM students";
+        
+         try (Connection conn = dbConnection.connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+             
+             while (rs.next()) {
+                 String name = rs.getString("name");
+                 String studentNumber = rs.getString("student_number");
+                 String programme = rs.getString("programme");
+                 String modulesEnrolled = rs.getString("modules_enrolled");
+                 String modulesCompleted = rs.getString("modules_completed");
+                 String modulesToRepeat = rs.getString("modules_to_repeat");
+                 
+                 reportBuilder.append("Name: ").append(name)
+                         .append("\nStudent Number: ").append(studentNumber)
+                         .append("\nProgramme: ").append(programme)
+                         .append("\nModules Enrolled: ").append(modulesEnrolled)
+                         .append("\nModules Completed: ").append(modulesCompleted)
+                         .append("\nModules To Repeat: ").append(modulesToRepeat)
+                         .append("\n\n");
+                         
+              }
+         } catch (SQLException e) {
+             e.printStackTrace();
+         }
+        return reportBuilder.toString();
     }
 }
+   
